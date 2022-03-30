ac0a0199135ca6cabcdfcdee9ebb5b94029a6834
tmelko-vidyo <tmelko@vidyo.com>
2022-03-30 15:59:19 +0300

    VidyoConnector 21.6.1.1 Library support

--- src/android/com/vidyo/vidyoconnector/VidyoActivity.java
+++ src/android/com/vidyo/vidyoconnector/VidyoActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.view.WindowManager;
@@ -12,6 +11,8 @@ import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
+
+import androidx.annotation.NonNull;
 
 import com.vidyo.VidyoClient.Connector.Connector;
 import com.vidyo.VidyoClient.Connector.ConnectorPkg;
